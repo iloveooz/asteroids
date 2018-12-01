de873e9293d779691b6b05b04460af59c3428a65
iloveooz <iloveooz@yandex.ru>
2018-12-01 22:11:03 +0700

    the main window is displayed

--- main.cxx
+++ main.cxx
@@ -6,16 +6,44 @@
 	const int W = 1200;
 	const int H = 800;
 	
+	float DEGTORAD = 0.017453f;
 	
 	int main() {
 		
+		// создаём окно
 		RenderWindow app(VideoMode(W, H), "Asteroids!");
 		app.setFramerateLimit(60);
 		
+		// создаём текстуры
 		Texture t1, t2;
 		
+		// загружаем текстуры из файлов
 		t1.loadFromFile("images/spaceship.png");
 		t2.loadFromFile("images/background.jpg");
+		
+		// создаём спрайты
+		Sprite sPlayer(t1), sBackground(t2);
+		// отобразить кусок текстуры
+		sPlayer.setTextureRect(IntRect(40, 0, 40, 40));
+		// точка, вокруг которой происходят все трансформации объекта
+		sPlayer.setOrigin(20, 20);
+		
+		// игровой цикл, пока открыто окно
+		while (app.isOpen()) {
+			// событие
+			Event event;
+			// пока приложение не закрыто
+			while (app.pollEvent(event)) {
+				if (event.type == Event::Closed)
+				app.close();
+			}
+			
+			/// отображение /// 
+			app.clear();
+			app.draw(sBackground);
+			app.draw(sPlayer);
+			app.display();
+		}
 		
 		
 		return 0;
