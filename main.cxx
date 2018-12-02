ecc75b0ace48cd4fa771420f2797221087347411
iloveooz <iloveooz@yandex.ru>
2018-12-03 02:17:55 +0700

    added animation

--- main.cxx
+++ main.cxx
@@ -1,12 +1,43 @@
 
 	#include <SFML/Graphics.hpp>
-	
+	#include <cmath>
+		
 	using namespace sf;
 	
 	const int W = 1200;
 	const int H = 800;
 	
 	float DEGTORAD = 0.017453f;
+	
+	class Animation {
+		public:
+		float Frame; 
+		float speed;
+		
+		Sprite sprite;
+		std::vector <IntRect> frames;
+		
+		Animation () {}
+		
+		Animation (Texture &t, int x, int y, int w, int h, int count, float Speed) {
+			Frame = 0;
+			speed = Speed;
+				 
+			for (int i = 0; i < count; i++) 
+				frames.push_back(IntRect(x + i * w, y, w, h));
+			
+			sprite.setTexture(t);
+			sprite.setOrigin(w / 2, h / 2);
+			sprite.setTextureRect(frames[0]);
+		}
+		
+		void update() {
+			Frame += speed;
+			int n = frames.size();
+			if (Frame >= n) Frame -= n;
+			if (n > 0) sprite.setTextureRect(frames[int(Frame)]);
+		}
+	};
 	
 	int main() {
 		
@@ -15,18 +46,41 @@
 		app.setFramerateLimit(60);
 		
 		// создаём текстуры
-		Texture t1, t2;
+		Texture t1, t2, t3, t4;
 		
 		// загружаем текстуры из файлов
 		t1.loadFromFile("images/spaceship.png");
 		t2.loadFromFile("images/background.jpg");
+		t3.loadFromFile("images/explosions/type_A.png");
+		t4.loadFromFile("images/rock.png");
 		
 		// создаём спрайты
-		Sprite sPlayer(t1), sBackground(t2);
+		Sprite sPlayer(t1);
+		Sprite sBackground(t2);
+		Sprite sExplosion(t3);
+		
 		// отобразить кусок текстуры
 		sPlayer.setTextureRect(IntRect(40, 0, 40, 40));
 		// точка, вокруг которой происходят все трансформации объекта
 		sPlayer.setOrigin(20, 20);
+		
+		// анимированный камень в космосе
+		Animation sRock(t4, 0, 0, 64, 64, 16, 0.2);
+		sRock.sprite.setPosition(400, 400);
+		
+		sExplosion.setPosition(300, 300);
+		
+		float Frame = 0;
+		float animSpeed = 0.4;
+		int frameCount = 20;
+				
+		float x = 300, y = 300;
+		float dx = 0, dy = 0, angle = 0;
+		
+		bool thrust;
+		
+		
+		
 		
 		// игровой цикл, пока открыто окно
 		while (app.isOpen()) {
@@ -38,10 +92,66 @@
 				app.close();
 			}
 			
+			// пример спрайтовой анимации
+			Frame += animSpeed;
+			if (Frame > frameCount) Frame -= frameCount;
+			sExplosion.setTextureRect(IntRect(int(Frame) * 50, 0, 50, 50));
+			
+			
+			// поворот вправо
+			if (Keyboard::isKeyPressed(Keyboard::Right)) angle += 3;
+			// поворот влево
+			if (Keyboard::isKeyPressed(Keyboard::Left)) angle -= 3;
+			// движение прямо
+			if (Keyboard::isKeyPressed(Keyboard::Up))
+				thrust = true;
+			else
+				thrust = false;
+			
+			// космический корабль - движение//	
+			if (thrust) {
+				dx += cos(angle * DEGTORAD) * 0.2;
+				dy += sin(angle * DEGTORAD) * 0.2; 
+			}
+			else {
+				dx *= 0.99;
+				dy *= 0.99;
+			}
+			
+			int maxSpeed = 15;
+			float speed = sqrt(dx * dx + dy * dy);
+			
+			if (speed > maxSpeed) {
+				dx *= maxSpeed / speed;
+				dy *= maxSpeed / speed;
+			}
+			
+			x += dx;
+			y += dy;
+			
+			if (x > W) x = 0;
+			if (x < 0) x = W;
+			
+			if (y > H) y = 0;
+			if (y < 0) y = H;
+			
+			sPlayer.setPosition(x, y);
+			sPlayer.setRotation(angle + 90);
+
+			sRock.update();
 			/// отображение /// 
+			
+			// очистка экрана
 			app.clear();
+			// отображение фона
 			app.draw(sBackground);
+			// отображение игрока
 			app.draw(sPlayer);
+			// отображение взрыва
+			app.draw(sExplosion);
+			// отображение камня
+			app.draw(sRock.sprite);
+			// отображает экран
 			app.display();
 		}
 		
