4db1c5ec058eb3458637c68386f6a8bf19d9fa9b
iloveooz <iloveooz@yandex.ru>
2019-04-16 23:32:58 +0700

    different changes

--- main.cxx
+++ main.cxx
@@ -3,10 +3,7 @@
 	#include <cmath>
 	#include <list>
 	#include <vector>
-	#include <deque>
 	#include <iostream>
-		
-	using namespace sf;
 
 	const int W = 1200;
 	const int H = 800;
@@ -14,42 +11,47 @@
 	float DEGTORAD = 0.017453f;
 
 
-	class Animation {
+	class Animation 
+	{
 		public:
 		float Frame; 
 		float speed;
 		
-		Sprite sprite;
-		std::vector <IntRect> frames;
+		sf::Sprite sprite;
+		std::vector <sf::IntRect> frames;
 	
 		Animation () {}
 		
-		Animation (Texture &t, int x, int y, int w, int h, int count, float Speed) {
+		Animation (sf::Texture &t, int x, int y, int w, int h, int count, float Speed)
+		{
 			Frame = 0;
 			speed = Speed;
 				 
 			for (int i = 0; i < count; i++) 
-				frames.push_back(IntRect(x + i * w, y, w, h));
+				frames.push_back(sf::IntRect(x + i * w, y, w, h));
 			
 			sprite.setTexture(t);
 			sprite.setOrigin(w / 2, h / 2);
 			sprite.setTextureRect(frames[0]);
 		}
 		
-		void update() {
+		void update()
+		{
 			Frame += speed;
 			int n = frames.size();
 			if (Frame >= n) Frame -= n;
 			if (n > 0) sprite.setTextureRect(frames[int(Frame)]);
 		}
 		
-		bool isEnd() {
+		bool isEnd()
+		{
 			return Frame + speed >= frames.size();
 		}
 	};
 
 
-	class Entity {
+	class Entity 
+	{
 		public:
 		float x, y;
 		float dx, dy;
@@ -57,12 +59,22 @@
 		
 		bool life = 1;
 		
+		// ускорение
+		bool thrust;
+		
+		// торможение
+		bool brake;
+		
 		std::string name;
 		Animation anim;
 		
-		Entity () { life = 1; }
+		Entity () 
+		{
+			life = 1;
+		}
 		
-		void settings (Animation &a, int X, int Y, float Angle = 0, int radius = 1) {
+		void settings (Animation &a, int X, int Y, float Angle = 0, int radius = 1) 
+		{
 			x = X;
 			y = Y;
 			anim = a;
@@ -70,27 +82,31 @@
 			R = radius;
 		}
 		
-		virtual void update() { }
+		virtual void update() {}
 		
-		void draw(RenderWindow &app) {
+		void draw(sf::RenderWindow &app) 
+		{
 			anim.sprite.setPosition(x, y);
 			anim.sprite.setRotation(angle + 90);
 			app.draw(anim.sprite);
 		}
 		
-		virtual ~Entity() { }
+		virtual ~Entity() {}
 	};
 
 
-	class asteroid : public Entity {
+	class asteroid : public Entity 
+	{
 		public:
-		asteroid () {
+		asteroid () 
+		{
 			dx = rand() % 8 - 4;
 			dy = rand() % 8 - 4;
 			name = "asteroid";
 		}
 		
-		void update() {
+		void update()
+		{
 			x += dx;
 			y += dy;
 			
@@ -103,14 +119,71 @@
 			
 	};
 
-
-	class bullet : public Entity {
+	class Player : public Entity
+	{
 		public:
-		bullet() {
+		Player() 
+		{
+			name = "player";
+			x = 300;
+			y = 300;
+			dx = 0;
+			dy = 0;
+			
+			angle = 0;
+		}
+		
+		void update() 
+		{
+			// космический корабль - ускорение	
+			if (thrust) 
+			{
+				dx += cos(angle * DEGTORAD) * 0.2;
+				dy += sin(angle * DEGTORAD) * 0.2; 
+			}
+			else 
+			{
+				dx *= 0.99;
+				dy *= 0.99;
+			}
+			
+			// космический корабль торможение и движение назад
+			if (brake) 
+			{
+				dx -= cos(angle * DEGTORAD) * 0.2;
+				dy -= sin(angle * DEGTORAD) * 0.2; 
+			}		
+			
+			int maxSpeed = 15;
+			float speed = sqrt(dx * dx + dy * dy);
+			
+			if (speed > maxSpeed)
+			{
+				dx *= maxSpeed / speed;
+				dy *= maxSpeed / speed;
+			}
+			
+			x += dx;
+			y += dy;
+			
+			if (x > W) x = 0;
+			if (x < 0) x = W;
+			
+			if (y > H) y = 0;
+			if (y < 0) y = H;
+		}
+	};
+
+	class bullet : public Entity 
+	{
+		public:
+		bullet()
+		{
 			name = "bullet";
 		}
 		
-		void update() {
+		void update()
+		{
 			dx = cos(angle * DEGTORAD) * 6;
 			dy = sin(angle * DEGTORAD) * 6;
 			x += dx;
@@ -120,26 +193,27 @@
 		
 	};
 
-	bool isCollide(Entity *a, Entity *b) { 
-		return 	(b->x - a->x) * (b->x - a->x) + 
-				(b->y - a->y) * (b->y - a->y) < (a->R + b->R) * (a->R + b->R);
+	bool isCollide(Entity *a, Entity *b)
+	{ 
+		return 	(b->x - a->x) * (b->x - a->x) + (b->y - a->y) * (b->y - a->y) < (a->R + b->R) * (a->R + b->R);
 	} 
 
 
-	int main() {
+	int main()
+	{
 		
 		srand(time(NULL));
 		
 		std::cout << "START!" << '\n';
 		
 		// создаём окно
-		RenderWindow app(VideoMode(W, H), "Asteroids!");
+		sf::RenderWindow app(sf::VideoMode(W, H), "Asteroids!");
 		app.setFramerateLimit(60);
 		
 		std::vector <int> vec;
 		
 		// создаём текстуры
-		Texture t1, t2, t3, t4, t5, t6;
+		sf::Texture t1, t2, t3, t4, t5, t6;
 		
 		// загружаем текстуры из файлов
 		t1.loadFromFile("images/spaceship.png");
@@ -153,37 +227,32 @@
 		t2.setSmooth(true);
 		
 		// создаём спрайты
-		Sprite sPlayer(t1);
-		Sprite sBackground(t2);
-		// Sprite sExplosion(t3);
-				
+		sf::Sprite sPlayer(t1);
+		sf::Sprite sBackground(t2);
+						
 		Animation sBullet(t5, 0, 0, 32, 64, 16, 0.8);
 		
 		// отобразить кусок текстуры
-		sPlayer.setTextureRect(IntRect(40, 0, 40, 40));
+		sPlayer.setTextureRect(sf::IntRect(40, 0, 40, 40));
+		
 		// точка, вокруг которой происходят все трансформации объекта
 		sPlayer.setOrigin(20, 20);
 		
 		// анимированный камень в космосе
 		Animation sRock(t4, 0, 0, 64, 64, 16, 0.2);
+		
 		// анимированный маленький камень в космосе
 		Animation sRockSmall(t6, 0, 0, 64, 64, 16, 0.4);
+		
 		// анимированный взрыв
 		Animation sExplosion(t3, 0, 0, 256, 256, 48, 0.6);
-		
-		//sRock.sprite.setPosition(400, 400);
-		
-		// sExplosion.setPosition(300, 300);
-		
-		
 		
 		// двусвязный список сущностей
 		std::list <Entity *> entities; 
 		
-		
-		
 		// создание астероидов
-		for (int i = 0; i < 25; i++) {
+		for (int i = 0; i < 25; i++) 
+		{
 			asteroid *a = new asteroid();
 			asteroid *sr = new asteroid();
 			a->settings(sRock, rand() % W, rand() % H, rand() % 360, 25);
@@ -193,54 +262,55 @@
 			entities.push_back(sr);
 		}
 		
-		// float Frame = 0;
-		// float animSpeed = 0.4;
-		// int frameCount = 20;
-				
-		float x = 300, y = 300;
-		float dx = 0, dy = 0, angle = 0;
+		Player *spaceShip = new Player;
 		
-		bool thrust;
+		entities.push_back(spaceShip);
 		
 		// игровой цикл, пока открыто окно
-		while (app.isOpen()) {
+		while (app.isOpen()) 
+		{
 			// событие
-			Event event;
+			sf::Event event;
 			// пока приложение не закрыто
-			while (app.pollEvent(event)) {
-				if (event.type == Event::Closed)
+			while (app.pollEvent(event)) 
+			{
+				if (event.type == sf::Event::Closed)
 				app.close();
 				
-				if (event.type == Event::KeyPressed) 
-					if (event.key.code == Keyboard::Space) {
+				if (event.type == sf::Event::KeyPressed) 
+					if (event.key.code == sf::Keyboard::Space)
+					{
 						bullet *b = new bullet();
 						b->settings(sBullet, x, y, angle, 10);
 						entities.push_back(b);
 					}
 				
 			}
-			
-			// пример спрайтовой анимации
-			// Frame += animSpeed;
-			
-			// if (Frame > frameCount) Frame -= frameCount;
-			// sExplosion.setTextureRect(IntRect(int(Frame) * 50, 0, 50, 50));
-			
+
 			// поворот вправо
-			if (Keyboard::isKeyPressed(Keyboard::Right)) angle += 3;
-			// поворот влево
-			if (Keyboard::isKeyPressed(Keyboard::Left)) angle -= 3;
-			// движение прямо
-			if (Keyboard::isKeyPressed(Keyboard::Up))
-				thrust = true;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))  
+				spaceShip->angle += 3;
+			 // поворот влево
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) 
+				spaceShip->angle -= 3;
+			 // движение прямо
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+				spaceShip->thrust = true;
 			else
-				thrust = false;
+				spaceShip->thrust = false;
+			
+			// движение назад и торможение
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+				spaceShip->brake = true;
+			else
+				spaceShip->brake = false;
 			
 			// цикл проверки на столкновение
 			for (auto a:entities) 
 				for (auto b:entities) 
 					if (a->name == "asteroid" && b->name == "bullet")
-						if (isCollide(a, b)) {
+						if (isCollide(a, b)) 
+						{
 							a->life = false;
 							b->life = false;
 							
@@ -253,51 +323,24 @@
 			
 			for (auto ex:entities)
 				if (ex->name == "explosion")
-					if (ex->anim.isEnd()) {
+					if (ex->anim.isEnd())
+					{
 						ex->life = 0;
 					}
 							
-			// if (Keyboard::isKeyPressed(Keyboard::Down)) thrust = false;
-			
-			// космический корабль - движение//	
-			if (thrust) {
-				dx += cos(angle * DEGTORAD) * 0.2;
-				dy += sin(angle * DEGTORAD) * 0.2; 
-			}
-			else {
-				dx *= 0.99;
-				dy *= 0.99;
-			}
-			
-			int maxSpeed = 15;
-			float speed = sqrt(dx * dx + dy * dy);
-			
-			if (speed > maxSpeed) {
-				dx *= maxSpeed / speed;
-				dy *= maxSpeed / speed;
-			}
-			
-			x += dx;
-			y += dy;
-			
-			if (x > W) x = 0;
-			if (x < 0) x = W;
-			
-			if (y > H) y = 0;
-			if (y < 0) y = H;
-			
 			sPlayer.setPosition(x, y);
 			sPlayer.setRotation(angle + 90);
 
-			// sRock.update();
-			/// отображение /// 
+			// отображение
 			
 			// цикл поиска и удаления погибших сущностей
-			for (auto i = entities.begin(); i != entities.end();) {
+			for (auto i = entities.begin(); i != entities.end();) 
+			{
 				Entity *e = *i;
 				e->update();
 				e->anim.update();
-				if (e->life == false) {
+				if (e->life == false) 
+				{
 					i = entities.erase(i);
 					delete e;
 				}
@@ -311,13 +354,7 @@
 			// отображение фона
 			app.draw(sBackground);
 			
-			// отображение игрока
-			app.draw(sPlayer);
-			// отображение взрыва
-			// app.draw(sExplosion);
-			
-			// отображение камней
-			// app.draw(sRock.sprite);
+			// отображение всех сущностей
 			for (auto i:entities) i->draw(app);
 			
 			// отображает экран
